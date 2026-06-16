e1616af95c53c62fd6ad798bb588a6ce7a2ee043
daniel berihun| <danielshitaye10@gmail.com>
2026-06-16 14:27:19 +0300

    style: adjust video grid to perfectly fit 2 videos

--- src/components/VideoShowcaseSection.tsx
+++ src/components/VideoShowcaseSection.tsx
@@ -71,7 +71,7 @@ export default function VideoShowcaseSection() {
         </div>
 
         {/* Video Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto">
           {videos.map((video, index) => (
             <div 
               key={index}
